e0717401a0158fa1f0106e69d193efc262fd6885
Salamon <ibrohim238@mail.ru>
2022-04-29 14:07:12 +0300

    removed unused links

--- app/Models/Manga.php
+++ app/Models/Manga.php
@@ -2,8 +2,6 @@
 
 namespace App\Models;
 
-use App\Filters\QueryFilter;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
